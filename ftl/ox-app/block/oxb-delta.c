9cde78fbb82bc1a7756579f671653f051f6a39f2
Benjamin Noer <benjaminmnoer25@gmail.com>
2020-06-06 11:10:28 +0200

    Removed queues from the delta component

--- ftl/ox-app/block/oxb-delta.c
+++ ftl/ox-app/block/oxb-delta.c
@@ -20,12 +20,9 @@ static struct app_channel **ch;
 
 struct delta_page {
     struct nvm_io_cmd          *nvme_cmd;
-    void                       *data; // is this correct? Maybe
+    void                       *data; // TODO: Double check.
 
-    // What do i hold?
     struct ox_mq_entry         *mentry;
-    STAILQ_ENTRY(lba_io_sec)    fentry;
-    TAILQ_ENTRY(lba_io_sec)     uentry;
 };
 
 struct delta_md_entry {
@@ -41,6 +38,7 @@ struct delta_md {
     uint64_t                    previous_chain;
 };
 
+// This is the write buffer struct.
 struct delta_cmd {
     struct nvm_io_cmd          *nvme_cmd;
     struct delta_page         **pages;
@@ -49,17 +47,7 @@ struct delta_cmd {
     uint16_t                    nmd;
 
     pthread_spinlock_t          spin;
-    STAILQ_ENTRY(delta_cmd)     fentry;
-    TAILQ_ENTRY(delta_cmd)      uentry;
 };
-
-STAILQ_HEAD(fpage_q, delta_page) fpagehead = STAILQ_HEAD_INITIALIZER(fpagehead);
-TAILQ_HEAD(upage_q, delta_page) upagehead = TAILQ_HEAD_INITIALIZER(upagehead);
-static pthread_spinlock_t page_spin;
-
-STAILQ_HEAD(fcmd_q, delta_cmd) fdeltahead = STAILQ_HEAD_INITIALIZER(fdeltahead);
-TAILQ_HEAD(ucmd_q, delta_cmd) udeltahead = TAILQ_HEAD_INITIALIZER(udeltahead);
-static pthread_spinlock_t cmd_spin;
 
 static void delta_mq_to (void **opaque, int c)
 {
@@ -79,37 +67,10 @@ static void delta_sq (struct ox_mq_entry *req)
     struct app_map_entry *map_entry;
     struct app_prov_ppas *prov;
 
-    // if (STAILQ_EMPTY(&fdeltahead))
-    //     return -1;
-
-    // pthread_spin_lock (&cmd_spin);
-    // cmdz = STAILQ_FIRST(&fdeltahead);
-    // if (!cmdz) {
-    //     pthread_spin_unlock (&cmd_spin);
-    //     retry--;
-    //     usleep (1000);
-    //     if (retry)
-    //         goto RETRY;
-    //     else
-    //         return -1;
-    // }
-
 
     
     map_entry = oxapp()->gl_map->read_fn(cmd->nvme_cmd->slba);
-    cmd->mentry = req;
-
-    // if (!map_entry->delta){
-    //     prov = oxapp()->gl_prov->new_fn (1, APP_LINE_DELTA); // For now, take first availabe block.
-
-    // }    
-
-RETRY:
-    // pthread_mutex_lock (&cmd->nvme_cmd->mutex);
-    // cmd->nvme_cmd->status.status = NVM_IO_FAIL;
-    // cmd->nvme_cmd->status.nvme_status = NVME_INTERNAL_DEV_ERROR;
-    // pthread_mutex_unlock (&cmd->nvme_cmd->mutex);
-    ox_mq_complete_req(delta_mq, cmd->mentry);
+    // TODO: Complete request. Use mentry from somewhere, possibly delta_page.
 }
 
 static void delta_sq_cb (void *opaque)
@@ -117,16 +78,8 @@ static void delta_sq_cb (void *opaque)
     printf("Have i done something correct? Delta MQ sq cb function called.\n");
     struct delta_cmd *cmd = (struct delta_cmd *) opaque;
     struct nvm_io_cmd *nvme_cmd = cmd->nvme_cmd;
-    // pthread_mutex_lock (&nvme_cmd->mutex);
 
     ox_ftl_callback (nvme_cmd);
-
-    // pthread_mutex_unlock (&nvme_cmd->mutex);
-
-    pthread_spin_lock (&cmd_spin);
-    TAILQ_REMOVE(&udeltahead, cmd, uentry);
-    STAILQ_INSERT_TAIL(&fdeltahead, cmd, fentry);
-    pthread_spin_unlock (&cmd_spin);
 }
 
 static void delta_stats_fill_row (struct oxmq_output_row *row, void *opaque)
@@ -158,9 +111,9 @@ static struct ox_mq_config delta_mq_config = {
 
 static void delta_callback(struct nvm_io_cmd *cmd)
 {
+    // TODO: What does this function do?
     struct delta_cmd *delta_cmd;
     delta_cmd = (struct delta_cmd *)cmd;
-    ox_mq_complete_req(delta_mq, delta_cmd->mentry);
 }
 
 static int delta_submit(struct nvm_io_cmd *cmd)
@@ -168,33 +121,6 @@ static int delta_submit(struct nvm_io_cmd *cmd)
     printf("Delta component received submit request.\n");
     uint32_t retry = DELTA_RETRY;
     struct delta_cmd *delta_cmd;
-
-RETRY:
-    pthread_spin_lock (&cmd_spin);
-    if (STAILQ_EMPTY(&fdeltahead)) {
-        pthread_spin_unlock (&cmd_spin);
-        retry--;
-        usleep (DELTA_RETRY_DELAY);
-        if (retry) {
-            goto RETRY;
-        } else
-            return -1; // TODO Requeue command if fail.
-    }
-
-    delta_cmd = STAILQ_FIRST(&fdeltahead);
-    if (!delta_cmd) {
-        pthread_spin_unlock (&cmd_spin);
-        retry--;
-        usleep (DELTA_RETRY_DELAY);
-        if (retry) {
-            goto RETRY;
-        } else
-            return -1; // TODO Requeue command if fail.
-    }
-
-    STAILQ_REMOVE_HEAD (&fdeltahead, fentry);
-    TAILQ_INSERT_TAIL(&udeltahead, delta_cmd, uentry);
-    pthread_spin_unlock (&cmd_spin);
 
     delta_cmd->nvme_cmd = cmd;
 
@@ -212,6 +138,7 @@ static void delta_exit()
 
 static int delta_init()
 {
+    // TODO: What is necessary to initialize?
     struct delta_cmd *cmd;
     struct delta_page *page;
     uint32_t cmd_i, page_i, ret;
@@ -228,38 +155,6 @@ static int delta_init()
         goto FREE_CH;
 
     npages = ch[0]->ch->geometry->pg_per_blk;
-
-    STAILQ_INIT(&fdeltahead);
-    TAILQ_INIT(&udeltahead);
-    STAILQ_INIT(&fpagehead);
-    TAILQ_INIT(&upagehead);
-
-    if (pthread_spin_init(&cmd_spin, 0)){
-        return -1;
-    }
-
-    if (pthread_spin_init(&page_spin, 0)){
-        return -1;
-    }
-    
-    for (cmd_i = 0; cmd_i < DELTA_CMDS; cmd_i++){
-        cmd = ox_calloc (sizeof (struct delta_cmd), 1, OX_MEM_OXBLK_DELTA);
-        if (!cmd) goto FREE_CMD;    
-
-        if (pthread_spin_init(&cmd->spin, 0)) {
-            ox_free (cmd, OX_MEM_OXBLK_DELTA);
-            goto FREE_CMD;
-        }
-
-        STAILQ_INSERT_TAIL(&fdeltahead, cmd, fentry);
-
-        for (page_i = 0; page_i < npages; page_i++){
-            page = ox_calloc (sizeof (struct delta_page), 1, OX_MEM_OXBLK_DELTA);
-            if (!page) goto FREE_CMD;
-
-            STAILQ_INSERT_TAIL(&fpagehead, page, fentry);
-        }
-    }
 
     /* Set thread affinity, if enabled */
     delta_mq_config.sq_affinity[0] = 0;
@@ -282,9 +177,6 @@ static int delta_init()
 
     return 0;
 
-FREE_CMD:
-    ox_free (cmd, OX_MEM_OXBLK_DELTA);
-    pthread_spin_destroy (&cmd->spin);
 FREE_CH:
     ox_free (ch, OX_MEM_OXBLK_LBA);
     return -1;
